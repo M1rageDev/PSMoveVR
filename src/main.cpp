0db9ca6a8f4a3dc384b0deb47e942730fa1bf0bd
MirageDev <forpixel.studio@gmail.com>
2024-03-22 22:13:03 +0100

    shit works

--- src/main.cpp
+++ src/main.cpp
@@ -20,20 +20,39 @@ int main()
 	// Create clock
 	psmovevr::Clock clock = psmovevr::Clock();
 
+	if (!vr::checkCamerasConnected()) {
+		// Exit if we have no cameras
+		printf("Error: No cameras connected.");
+		exit(1);
+	}
+
 	// Create UDP connection
 	vr::VRConnection connection = vr::VRConnection(49000);
 
 	// Initialize
-	psmovevr::loadCameras();
-	psmovevr::loadControllers();
+	vr::VRCamera* cameras = vr::initializeEveryCam(60, 640, 480, 150, 100, false);
+
+	// Create controller handlers
+	const char* leftSrl = "00:06:f7:c9:a1:fb";
+	const char* rightSrl = "00:06:f7:c9:a1:fb";
+	vr::VRControllerHandler controllers = vr::VRControllerHandler();
+	psmoveapi::PSMoveAPI moveAPI(&controllers);
+
+	// Set pointers to the left/right controller
+	vr::VRController* leftController = nullptr;
+	vr::VRController* rightController = nullptr;
+	for (int i = 0; i < controllers.controllers.size(); i++) {
+		if (controllers.controllers[i].serial == leftSrl) leftController = &controllers.controllers[i];
+		else if (controllers.controllers[i].serial == rightSrl) rightController = &controllers.controllers[i];
+	}
 
 	// Calibrate controller IMUs
-	psmovevr::calibrateImu(5000);
+	vr::calibrateControllers(5000, &controllers, &moveAPI);
 
 	// Set colors
-	psmovevr::setControllerColor(0, { 0.f, 1.f, 1.f });
-	psmovevr::setControllerColor(1, { 1.f, 0.f, 1.f });
-	psmovevr::tickControllers();
+	leftController->color = { 0.f, 1.f, 1.f };
+	rightController->color = { 1.f, 0.f, 1.f };
+	moveAPI.update();
 
 	// Main loop
 	for (;;) {
@@ -41,18 +60,18 @@ int main()
 		clock.tick();
 
 		// Tick cameras and controllers
-		psmovevr::readCameras();
-		psmovevr::tickControllers();
+		cameras[0].read();
+		moveAPI.update();
 
 		// Debug info
-		cv::putText(psmovevr::getPicture(0), std::to_string(1.f / clock.timestep), cv::Point(0.f, 16.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(255.f, 255.f, 255.f));
+		//cv::putText(psmovevr::getPicture(0), std::to_string(1.f / clock.timestep), cv::Point(0.f, 16.f), cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(255.f, 255.f, 255.f));
 
 		// Send UDP signal to SteamVR
 		//std::string stringBuffer = std::vformat(DATA_BUFFER, std::make_format_args());
 		//const char* charBuffer = stringBuffer.c_str();
-		connection.send("Mm bobbies");
+		//connection.send("Mm bobbies");
 
-		cv::imshow("Camera", psmovevr::getPicture(0));
+		cv::imshow("Camera", cameras[0].cvImg);
 		int key = cv::waitKey(1);
 		if (key == 27) break;
 	}
@@ -60,6 +79,6 @@ int main()
 	// Stop the service
 	connection.stop();
 	cv::destroyAllWindows();
-	psmovevr::stopCameras();
+	vr::stopEveryCam(cameras);
 	return 0;
 }
